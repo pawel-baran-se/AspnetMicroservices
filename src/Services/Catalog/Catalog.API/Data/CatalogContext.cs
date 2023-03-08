0c0be3b7135334a03845734151ad53bb8cde30be
Pawel Baran <pawelbaranc@gmail.com>
2023-03-08 20:45:38 +0100

    Entities and Context created Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+	public class CatalogContext : ICatalogContext
+	{
+		public CatalogContext(IConfiguration configuration)
+		{
+			var mongoClient = new MongoClient(
+				configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+			var mongoDatabase = mongoClient.GetDatabase(
+				configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+			Products = mongoDatabase.GetCollection<Product>(
+				configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+			CatalogContextSeed.SeedData(Products);
+		}
+
+
+		public IMongoCollection<Product> Products { get; }
+	}
+}
