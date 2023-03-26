ded3515651086559bfa22d072e2cce0c8be19196
Pawel Baran <pawelbaranc@gmail.com>
2023-03-26 09:24:21 +0200

    Discount.Proto Added

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,17 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+
+namespace Discount.Grpc.Services
+{
+	public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+	{
+		private readonly ILogger<DiscountService> _logger;
+		private readonly IDiscountRepository _repository;
+
+		public DiscountService(ILogger<DiscountService>  logger,IDiscountRepository repository)
+		{
+			_logger = logger;
+			_repository = repository;
+		}
+	}
+}
