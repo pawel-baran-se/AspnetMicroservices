a5e30cc88d1301e6b7ea551b42bea1f808185bd7
Pawel Baran <pawelbaranc@gmail.com>
2023-03-31 19:50:14 +0200

    Fixed location

--- src/Services/Ordering/Ordering.Domain/Entities/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -1,11 +1,11 @@
-﻿using Ordering.Application.Common;
+﻿using Ordering.Domain.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ordering.Application.Entities
+namespace Ordering.Domain.Entities
 {
 	public class Order : EntityBase
 	{
