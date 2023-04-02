75df5dc5aaaba50b5b8d1ca7fcc8099a49ae79d5
Pawel Baran <pawelbaranc@gmail.com>
2023-04-02 13:04:35 +0200

    Chechkout Command developed

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+	public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+	{
+		public CheckoutOrderCommandValidator()
+		{
+			RuleFor(c => c.UserName)
+				.NotEmpty().WithMessage("{UserName} is required")
+				.NotNull()
+				.MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
+		}
+	}
+}
