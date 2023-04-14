cb6216c16d7c690f96d983c37c2c676f9dd0e17b
Pawel Baran <pawelbaranc@gmail.com>
2023-04-14 20:50:50 +0200

    Developed Ocelot Api Gateway

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+	loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+	loggingBuilder.AddConsole();
+	loggingBuilder.AddDebug();
+});
+
+builder.Services.AddOcelot();
+
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
